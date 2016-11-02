507f9dd79d5f9757ac36e341b83f6772e604c5f0
somonus <312677182@qq.com>
2016-11-02 15:32:58 +0800

    update for iOS

--- example/index.ios.js
+++ example/index.ios.js
@@ -11,23 +11,17 @@ import {
   Text,
   View
 } from 'react-native';
-import { Router, Route } from './src';
-import Test from './test';
-import Demo from './demo';
+import Speech from 'react-native-speech';
 
 export default class app2 extends Component {
   render() {
+    Speech.speak('欢迎欢迎');
     return (
-      <Router>
-        <Route path='root' component={Test}>
-          <Route path='demo' component={Demo} animation='FloatFromRight'>
-            <Route path='demo' component={Demo} animation='FloatFromLeft'></Route>
-          </Route>
-        </Route>
-        <Route path='demo' component={Demo} animation='FloatFromBottom'>
-          <Route path='demo' component={Demo}></Route>
-        </Route>
-      </Router>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+      </View>
     );
   }
 }
